5f61c6fd1d8859edea2b92d744987bfa9dab6520
Blasko-Peter <blaskopeter@t_online.hu>
2019-06-27 11:47:31 +0200

    add postgresql database to project and I can send data from front end to back end

--- src/main/java/com/blasko/shop/controller/HomeController.java
+++ src/main/java/com/blasko/shop/controller/HomeController.java
@@ -11,12 +11,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = {"/"})
 public class HomeController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("product/index.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String suppliername = req.getParameter("suppliername");
+        String supplierdescription = req.getParameter("supplierdescription");
+        System.out.println(suppliername + "/" + supplierdescription);
+
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         engine.process("product/index.html", context, resp.getWriter());
