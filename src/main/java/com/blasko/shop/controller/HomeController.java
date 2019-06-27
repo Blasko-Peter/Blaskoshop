d3076c55e5135877a023155e3348ec55b89c6da1
Blasko-Peter <blaskopeter@t_online.hu>
2019-06-27 13:58:25 +0200

    refactor: supplier goes to /admin/supplier route and I have a home route which is get the index.html

--- src/main/java/com/blasko/shop/controller/HomeController.java
+++ src/main/java/com/blasko/shop/controller/HomeController.java
@@ -1,10 +1,6 @@
 package com.blasko.shop.controller;
 
 import com.blasko.shop.config.TemplateEngineUtil;
-
-import com.blasko.shop.dao.SupplierDao;
-import com.blasko.shop.dao.implementation.SupplierDaoMem;
-import com.blasko.shop.model.Supplier;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -14,26 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = {"/"})
 public class HomeController extends HttpServlet {
 
-    SupplierDao sd = SupplierDaoMem.getInstance();
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        WebContext context = new WebContext(req, resp, req.getServletContext());
-        engine.process("product/index.html", context, resp.getWriter());
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String suppliername = req.getParameter("suppliername");
-        String supplierdescription = req.getParameter("supplierdescription");
-        Supplier newSupplier = new Supplier(suppliername, supplierdescription);
-        sd.add(newSupplier);
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         engine.process("product/index.html", context, resp.getWriter());
